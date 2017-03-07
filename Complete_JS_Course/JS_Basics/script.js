359b7978c82265cfff67aa92d8063a5f283962c9
Amy Woodworth <woodworthcommunications@gmail.com>
2017-03-07 11:14:04 -0600

    Review of arrays

--- Complete_JS_Course/JS_Basics/script.js
+++ Complete_JS_Course/JS_Basics/script.js
@@ -266,9 +266,30 @@ if (x === 5) {
 ////////////////////////////////////////
 // Arrays
 
+var names = ['John', 'Jane', 'Mark'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names[2]);
+names[1] = 'Ben';
+console.log(names);
+
+var john = ['John', 'Smith', 1990, 'designer', false];
+john.push('blue');
+john.unshift('Mr.');
+john.pop();
+john.shift();
+
+console.log(john);
+
+// alert(john.indexOf('Smith'));
+
+if (john.indexOf('teacher') === -1) {
+  console.log('John is NOT a teacher.')
+}
 
 
-
+////////////////////////////////////////
+// Objects
 
 
 
