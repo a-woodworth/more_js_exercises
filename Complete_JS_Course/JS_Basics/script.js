603d998651bf7229585df37b457a7ae187b12a0c
Amy Woodworth <woodworthcommunications@gmail.com>
2017-03-08 15:51:34 -0600

    Finish basics section.

--- Complete_JS_Course/JS_Basics/script.js
+++ Complete_JS_Course/JS_Basics/script.js
@@ -266,34 +266,246 @@ if (x === 5) {
 ////////////////////////////////////////
 // Arrays
 
-var names = ['John', 'Jane', 'Mark'];
-var years = new Array(1990, 1969, 1948);
+// var names = ['John', 'Jane', 'Mark'];
+// var years = new Array(1990, 1969, 1948);
 
-console.log(names[2]);
-names[1] = 'Ben';
-console.log(names);
+// console.log(names[2]);
+// names[1] = 'Ben';
+// console.log(names);
 
-var john = ['John', 'Smith', 1990, 'designer', false];
-john.push('blue');
-john.unshift('Mr.');
-john.pop();
-john.shift();
+// var john = ['John', 'Smith', 1990, 'designer', false];
+// john.push('blue');
+// john.unshift('Mr.');
+// john.pop();
+// john.shift();
 
-console.log(john);
+// console.log(john);
 
-// alert(john.indexOf('Smith'));
+// // alert(john.indexOf('Smith'));
 
-if (john.indexOf('teacher') === -1) {
-  console.log('John is NOT a teacher.')
-}
+// if (john.indexOf('teacher') === -1) {
+//   console.log('John is NOT a teacher.')
+// }
 
 
 ////////////////////////////////////////
 // Objects
 
+// var john = {
+//   name: 'John',
+//   lastName: 'Smith',
+//   yearOfBirth: 1990,
+//   job: 'teacher',
+//   isMarried: false
+// };
+
+// console.log(john);
+// console.log(john.lastName);
+// console.log(john['lastName']);
+
+// var xyz = 'job';
+// console.log(john[xyz]);
+
+// john.lastName = 'Miller';
+// john['job'] = 'programmer';
+
+// console.log(john);
+
+// var jane = new Object();
+// jane.name = 'Jane';
+// jane.lastName = 'Smith';
+// jane['yearOfBirth'] = 1969;
+// jane['job'] = 'retired';
+// jane['isMarried'] = true;
+
+// console.log(jane);
 
 
+////////////////////////////////////////
+// Objects and methods
+
+/* v1.0
+var john = {
+  name: 'John',
+  lastName: 'Smith',
+  yearOfBirth: 1990,
+  job: 'teacher',
+  isMarried: false,
+  family: ['Jane', 'Mark', 'Bob'],
+  // function expression
+  calculateAge: function() {
+    return 2017 - this.yearOfBirth;
+  }
+};
+*/
+
+// console.log(john);
+// console.log(john.family);
+// console.log(john.family[2]);
+// console.log(john.calculateAge());
+
+// var age = john.calculateAge();
+// john.age = age;
+// console.log(john);
+
+//v2.0
+// var john = {
+//   name: 'John',
+//   lastName: 'Smith',
+//   yearOfBirth: 1990,
+//   job: 'teacher',
+//   isMarried: false,
+//   family: ['Jane', 'Mark', 'Bob'],
+//   calculateAge: function() {
+//     // return 2017 - this.yearOfBirth;
+//     this.age = 2017 - this.yearOfBirth;
+//   }
+// };
+
+// john.calculateAge();
+// console.log(john);
+
+// var mike = {
+//   yearOfBirth: 1950,
+//   calculateAge: function() {
+//     this.age = 2017 - this.yearOfBirth;
+//   }
+// };
+
+// mike.calculateAge();
+// console.log(mike);
 
 
+////////////////////////////////////////
+// Loops
+
+//for loops
+// for (var i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+/*
+0, true, print 0, update i to 1
+1, true, print 1, update i to 2
+.
+.
+.
+9, true, print 9, update i to 10
+10, FALSE, end loop!
+*/
+
+// var names = ['John', 'Jane', 'Mary', 'Mark', 'Bob'];
+/*
+for (var i = 0; i < names.length; i++) {
+  console.log('Hi ' + names[i] + '!');
+}
+
+// backward count
+for (var i = names.length - 1; i >= 0; i--) {
+  console.log(names[i]);
+}
+*/
+//while loop
+/*
+var i = 0;
+while(i < names.length) {
+  console.log(names[i]);
+  i++;
+}
+
+for (var i = 1; i <= 5; i++) {
+  console.log(i);
+  if (i === 3) {
+    break;
+  }
+}
+
+for (var i = 1; i <= 5; i++) {
+  if (i === 3) {
+    continue;
+  }
+  console.log(i);
+}
+*/
 
 
+////////////////////////////////////////
+// Coding Challenge 2
+
+/*
+1. Create an array with some years where
+persons were born
+2. Create an empty array (just [])
+3. Use a loop to fill the array with the ages
+of the persons
+4. Use another loop to log into the console
+whether each person is of full age (18 or
+older), as well as their age
+5. Finally, create a function called
+printFullAge which receives the vector of
+years as an argument, executes the steps 2.,
+3., and 4. and returns a vector of true/false
+boolean values: true if the person is of full
+age (>= 18 years) and false if not (< 18
+years)
+6. Call the function with two different
+vectors and store the results in two
+variables: full_1 and full_2
+
+Example input: [1965, 2008, 1992]
+Example output: [true, false, true]
+
+Hint: you can use a loop not only to read from
+an array, like y[i], but also to set values in
+an array, like y[i] = ... You can also use the
+specific array methods.
+*/
+
+// 1.
+var birthYear = [1965, 2008, 1992];
+
+// 2.
+var ages = [];
+
+// 3.
+// my solution
+for (i = 0; i < birthYear.length; i++) {
+  ages.push(2017 - birthYear[i]);
+}
+
+// exercise solution
+// for (i = 0; i < birthYear.length; i++) {
+//   ages[i] = 2017 - birthYear[i];
+// }
+
+// 4.
+for (i = 0; i < ages.length; i++) {
+  if (ages[i] >= 18) {
+    console.log('Person ' + (i + 1) + ' is ' + ages[i] + ' years old and 18 or older.')
+  } else {
+    console.log('Person ' + (i + 1) + ' is ' + ages[i] + ' years old and younger than 18.');
+  }
+}
+
+// 5.
+function printFullAge(years) {
+  var fullAge = [];
+  for (i = 0; i < years.length; i++) {
+    var age = 2017 - years[i];
+    if (age >= 18) {
+      fullAge.push(true);
+    }
+    else {
+      fullAge.push(false);
+    }
+  }
+  return fullAge;
+}
+console.log(printFullAge([1965, 2008, 1992]));
+
+// 6.
+var full_1 = printFullAge([1965, 2008, 1992]);
+var full_2 = printFullAge([2009, 1977, 2011]);
+
+console.log(full_1);
+console.log(full_2);
