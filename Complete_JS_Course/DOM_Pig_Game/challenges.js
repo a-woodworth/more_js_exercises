d2b9cba301f6b8cdc1a48f0d68b52742a55ea59f
Amy Woodworth <woodworthcommunications@gmail.com>
2017-03-26 15:32:23 -0500

    Finish challenge 2

--- Complete_JS_Course/DOM_Pig_Game/challenges.js
+++ Complete_JS_Course/DOM_Pig_Game/challenges.js
@@ -51,8 +51,17 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     // Update the UI
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
+    var finalScoreInput = document.querySelector('.final-score').value;
+    var winningScore;
+
+    if(finalScoreInput) {
+      winningScore = finalScoreInput;
+    } else {
+      winningScore = 100;
+    }
+
     // Check if player won the game
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= winningScore) {
       document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
       document.querySelector('.dice').style.display = 'none';
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
