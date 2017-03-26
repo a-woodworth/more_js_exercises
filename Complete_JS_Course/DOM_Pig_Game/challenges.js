b2e7d656017ffddcc5720ebf8276cbe95a7e66a5
Amy Woodworth <woodworthcommunications@gmail.com>
2017-03-26 16:49:38 -0500

    Finish challenge 3

--- Complete_JS_Course/DOM_Pig_Game/challenges.js
+++ Complete_JS_Course/DOM_Pig_Game/challenges.js
@@ -16,30 +16,24 @@ init();
 document.querySelector('.btn-roll').addEventListener('click', function() {
   if (gamePlaying) {
     // 1. Get random number
-    var dice = Math.floor(Math.random() * 6) + 1;
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
 
     // 2. Display result
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
+    document.getElementById('dice-1').style.display = 'block';
+    document.getElementById('dice-2').style.display = 'block';
+    document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+    document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
 
     // 3. Update the round score IF the rolled number was NOT a 1
-    if (dice === 6 && lastRoll === 6) {
-      // Player loses score
-      scores[activePlayer] = 0;
-      document.querySelector('#score-' + activePlayer).textContent = '0';
-      nextPlayer();
-    } else if (dice !== 1) {
+    if (dice1 !== 1 && dice2 !== 1) {
       // Add score
-      roundScore += dice;
+      roundScore += dice1 + dice2;
       document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
       // Next player
       nextPlayer();
     }
-
-    lastRoll = dice;
-    // console.log(lastRoll);
   }
 });
 
@@ -63,7 +57,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     // Check if player won the game
     if (scores[activePlayer] >= winningScore) {
       document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-      document.querySelector('.dice').style.display = 'none';
+      hideDice();
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
       document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
       gamePlaying = false;
@@ -84,7 +78,7 @@ function nextPlayer() {
   document.querySelector('.player-0-panel').classList.toggle('active');
   document.querySelector('.player-1-panel').classList.toggle('active');
 
-  document.querySelector('.dice').style.display = 'none';
+  hideDice();
 }
 
 document.querySelector('.btn-new').addEventListener('click', init);
@@ -95,8 +89,7 @@ function init() {
   roundScore = 0;
   gamePlaying = true;
 
-  document.querySelector('.dice').style.display = 'none';
-
+  hideDice();
   document.getElementById('score-0').textContent = '0';
   document.getElementById('score-1').textContent = '0';
   document.getElementById('current-0').textContent = '0';
@@ -108,6 +101,11 @@ function init() {
   document.querySelector('.player-0-panel').classList.remove('active');
   document.querySelector('.player-1-panel').classList.remove('active');
   document.querySelector('.player-0-panel').classList.add('active');
+}
+
+function hideDice() {
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
 }
 
 /*
